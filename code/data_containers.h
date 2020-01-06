8a715c1db7e88ea89cc2ca27588c1b1b96e0ecf1
PowerTravel <Jakob.hariz@gmail.com>
2020-01-06 16:34:15 +0100

    Add mergeSort to list

--- code/data_containers.h
+++ code/data_containers.h
@@ -2,6 +2,9 @@
 
 #include "memory.h"
 
+// Todo: Make all lists return Pointers, not copies.
+// Todo: Move list to use Iterators instead of first(), Last(), Next() Etc
+
 template <class T>
 class list
 {
@@ -12,104 +15,354 @@ class list
     entry* Next;
   };
 
-  entry* Sentinel;
-  entry* Position;
+  entry* mSentinel;
+  entry* mPosition;
 
-  memory_arena* Arena;
+  memory_arena* mArena;
 
-  u32 Size;
+  u32 mSize;
+
+  void InitializeSentinel( entry* Sentinel )
+  {
+    *Sentinel = {};
+    Sentinel->Next     = Sentinel;
+    Sentinel->Previous = Sentinel;
+  }
 
   void InsertBetween( entry* A, entry* B, entry* NewEntry )
   {
-    NewEntry->Previous = A;
     NewEntry->Next = B;
+    NewEntry->Next->Previous = NewEntry;
+    NewEntry->Previous = A;
+    NewEntry->Previous->Next = NewEntry;
+  }
 
-    if(A)
-    {
-      A->Next = NewEntry;
-    }
-
-    if(B)
-    {
-      B->Previous =NewEntry;
-    }
-
-    Position = NewEntry;
-
-    ++Size;
+  void Dissconect( entry* Entry )
+  {
+    Entry->Previous->Next = Entry->Next;
+    Entry->Next->Previous = Entry->Previous;
+    Entry->Next = 0;
+    Entry->Previous = 0;
   }
 
   entry* AllocateNewEntry(const T& Data)
   {
-    entry* Result = (entry*) PushStruct(Arena, entry);
+    entry* Result = (entry*) PushStruct(mArena, entry);
     Result->Data = Data;
     return Result;
   }
 
-  public:
-    list() : Sentinel(0), Position(0), Arena(0), Size(0){};
+  inline void
+  RealignSentinel( entry* Sentinel )
+  {
+    // Any function that returns a entry (not entry*) needs to be realigned.
+    // The returned copy points to the first and last entry as it should.
+    // However, Since the returned entry is a copy it has a different address
+    // thus the First and Last Entry Needs to have their pointers reset to
+    // the returned copy.
+    Sentinel->Next->Previous = Sentinel;
+    Sentinel->Previous->Next = Sentinel;
+  };
 
-    list(memory_arena* aArena) : Sentinel(0), Position(0), Arena(0), Size(0)
+  void Split(entry* InitialSentinel, const u32 InitialCount,
+             entry* SentinelA, u32* CountA,
+             entry* SentinelB, u32* CountB)
+  {
+    InitializeSentinel(SentinelA);
+    InitializeSentinel(SentinelB);
+
+    *CountA = Roof(InitialCount/2);
+    *CountB = InitialCount - *CountA;
+
+    if((*CountA==0) && (*CountB==0))
+    {
+      return;
+    }
+
+    entry* Pos = InitialSentinel;
+    for(u32 i = 0; i < *CountA; ++i)
+    {
+      Pos = Pos->Next;
+    }
+
+    entry* ALast  = Pos;
+    entry* BFirst = Pos->Next;
+
+
+    SentinelA->Next = InitialSentinel->Next;
+    SentinelA->Next->Previous = SentinelA;
+    SentinelA->Previous = ALast;
+    SentinelA->Previous->Next = SentinelA;
+
+    if(*CountB!=0)
+    {
+      SentinelB->Next = BFirst;
+      SentinelB->Next->Previous = SentinelB;
+      SentinelB->Previous = InitialSentinel->Previous;
+      SentinelB->Previous->Next = SentinelB;
+    }
+  }
+
+  entry Merge( entry* SentinelA,      u32* CountA,
+               entry* SentinelB,      u32* CountB,
+               s32 (*DifferenceFun) (const T*, const T*) )
+  {
+    entry* PositionA = SentinelA->Next;
+    entry* PositionB = SentinelB->Next;
+
+    entry ResultSentinel = {};
+    InitializeSentinel(&ResultSentinel);
+    entry* Position = &ResultSentinel;
+
+    u32 TotCount = *CountA+*CountB;
+    u32 Count = 0;
+    while(Count < TotCount)
+    {
+      if( (PositionA != SentinelA) &&
+          (PositionB != SentinelB))
+      {
+        entry* EntryToInsert = 0;
+        s32 DiffVal = DifferenceFun(&PositionA->Data, &PositionB->Data);
+        if(DiffVal >= 0)
+        {
+          EntryToInsert = PositionA;
+          PositionA = PositionA->Next;
+        }else{
+          EntryToInsert = PositionB;
+          PositionB = PositionB->Next;
+        }
+
+        Dissconect(EntryToInsert);
+        InsertBetween( Position, Position->Next, EntryToInsert);
+        Position = EntryToInsert;
+        ++Count;
+      }else{
+        if( (PositionA != SentinelA) )
+        {
+          ResultSentinel.Previous->Next = SentinelA->Next;
+          SentinelA->Next->Previous = ResultSentinel.Previous;
+
+          SentinelA->Previous->Next = &ResultSentinel;
+          ResultSentinel.Previous  = SentinelA->Previous;
+
+          Count = TotCount;
+          *SentinelA = {};
+        }else{
+          ResultSentinel.Previous->Next = SentinelB->Next;
+          SentinelB->Next->Previous = ResultSentinel.Previous;
+
+          SentinelB->Previous->Next = &ResultSentinel;
+          ResultSentinel.Previous  = SentinelB->Previous;
+
+          Count = TotCount;
+          *SentinelB = {};
+        }
+      }
+    }
+
+    return ResultSentinel;
+  }
+
+  entry MergeSort( entry* Sentinel, u32 Count, s32 (*DifferenceFun) (const T*, const T*)  )
+  {
+    entry SentinelA, SentinelB;
+    u32 CountA ,CountB;
+
+    Split(  Sentinel,   Count,
+           &SentinelA, &CountA,
+           &SentinelB, &CountB);
+
+    entry ResultA = {};
+    if(CountA > 1)
+    {
+      ResultA = MergeSort(&SentinelA, CountA, DifferenceFun);
+    }else{
+      ResultA = SentinelA;
+    }
+
+    RealignSentinel(&ResultA);
+
+    entry ResultB = {};
+    if(CountB > 1)
+    {
+      ResultB = MergeSort(&SentinelB, CountB, DifferenceFun);
+    }else{
+      ResultB = SentinelB;
+    }
+    RealignSentinel(&ResultB);
+
+    entry Result = Merge( &ResultA, &CountA,
+                          &ResultB, &CountB,
+                          DifferenceFun);
+    RealignSentinel(&Result);
+
+    return Result;
+  }
+
+  public:
+    list() : mSentinel(0), mPosition(0), mArena(0), mSize(0){};
+
+    list(memory_arena* aArena) : mSentinel(0), mPosition(0), mArena(0), mSize(0)
     {
       Initiate(aArena);
     }
 
-    void Initiate( memory_arena* aArena )
+    void MergeSort( s32 (*DifferenceFun) (const T*, const T*) )
     {
-      Arena = aArena;
-      Sentinel = (entry*) PushStruct(Arena, entry);
-      Sentinel->Previous = Sentinel;
-      Sentinel->Next = Sentinel;
-      Position = Sentinel;
+      if(mSize <= 1)
+      {
+        return;
+      }
+      *mSentinel = MergeSort(mSentinel, mSize, DifferenceFun);
+      RealignSentinel(mSentinel);
+      First();
     }
 
-    u32 GetSize() { return Size; };
-    b32 IsEmpty() { return ( Size==0 ); }
-    b32 IsEnd()   { return (Position == Sentinel); };
-
-
-    void First()   { Position = Sentinel->Next;     };
-    void Last()    { Position = Sentinel->Previous; };
-    void Next()    { if(!IsEnd()) { Position = Position->Next;     } };
-    void Previous(){ if(!IsEnd()) { Position = Position->Previous; } };
-
-    void InsertBefore(const T& Data)
+    void DeleteDuplicates( b32 (*EqualityFun) (const T *, const T *) )
     {
-      entry* NewEntry = AllocateNewEntry( Data );
-      InsertBetween( Position->Previous, Position, NewEntry );
+      entry* ScannerA = First();
+      while(ScannerA != aSentinel)
+      {
+        entry* ScannerB = ScannerA->Next;
+        while(ScannerB != aSentinel)
+        {
+          if(EqualityFun(&ScannerA->Data, &ScannerB->Data))
+          {
+            entry* Duplicate = ScannerB;
+            ScannerB = ScannerB->Previous;
+            Dissconect( Duplicate );
+          }
+
+          ScannerB = ScannerB->Next;
+        }
+        ScannerA = ScannerA->Next;
+      }
+    }
+
+    void Initiate( memory_arena* aArena )
+    {
+      mArena = aArena;
+      mSentinel = (entry*) PushStruct(mArena, entry);
+      InitializeSentinel(mSentinel);
+      mPosition = mSentinel;
+    }
+
+    u32 GetSize() { return mSize; };
+    b32 IsEmpty() { return ( mSize==0 ); }
+    b32 IsEnd()   { return (mPosition == mSentinel); };
+
+    T* First()
+    {
+      mPosition = mSentinel->Next;
+      return &mPosition->Data;
     };
 
-    void InsertAfter(const T& Data)
+    T* Last()
+    {
+      mPosition = mSentinel->Previous;
+      return &mPosition->Data;
+    };
+
+    T* Next()
+    {
+      if(!IsEnd())
+      {
+        mPosition = mPosition->Next;
+      }
+      return &mPosition->Data;
+    };
+
+    T* Previous()
+    {
+      if(!IsEnd())
+      {
+        mPosition = mPosition->Previous;
+      }
+      return &mPosition->Data;
+    };
+
+    T* InsertBefore(const T& Data)
     {
       entry* NewEntry = AllocateNewEntry( Data );
-      InsertBetween( Position, Position->Next, NewEntry );
+      InsertBetween( mPosition->Previous, mPosition, NewEntry );
+      mPosition = NewEntry;
+      ++Size;
+      return &Position->Data;
+    };
+
+    T* InsertAfter(const T& Data)
+    {
+      entry* NewEntry = AllocateNewEntry( Data );
+      InsertBetween( mPosition, mPosition->Next, NewEntry );
+      mPosition = NewEntry;
+      ++mSize;
+      return &mPosition->Data;
+    }
+
+    b32 PushBackUnique(const T& Data, b32 (*EqualityFun) (const T *, const T *) )
+    {
+      entry* Scanner = aSentinel->Next;
+      while(Scanner != aSentinel)
+      {
+        if(EqualityFun(&Data, Scanner))
+        {
+          return false;
+        }
+      }
+
+      entry* NewEntry = AllocateNewEntry( Data );
+      InsertBetween( mSentinel->Previous, mSentinel, NewEntry );
+      ++mSize;
+      return true;
     }
 
     void Remove()
     {
       if(IsEnd()){ return; }
 
-      entry* Base = Position->Previous;
-      entry* Next = Position->Next;
+      entry* Base = mPosition->Previous;
+      entry* Next = mPosition->Next;
 
       Base->Next = Next;
       Next->Previous = Base;
 
-      Position = Base;
-      if(Position == Sentinel)
+      mPosition = Base;
+      if(mPosition == mSentinel)
       {
-        Position = Sentinel->Next;
+        mPosition = mSentinel->Next;
       }
 
-      --Size;
+      --mSize;
     };
 
     T Get()
     {
-      return Position->Data;
+      return mPosition->Data;
+    };
+
+    T* GetRef()
+    {
+      return &mPosition->Data;
+    };
+
+
+    T Get(u32 Idx)
+    {
+      Assert(Idx < GetSize());
+      u32 i = 0;
+      for (First(); !IsEnd(); Next())
+      {
+        if(i==Idx)
+        {
+          ++i;
+          return Get();
+        }
+      }
+
+      INVALID_CODE_PATH;
+      return {};
     };
 };
-
 
 template <class T>
 class fifo_queue
@@ -254,7 +507,7 @@ class hash_map
   vector< list< pair* > > Map;
 
   public:
-    hash_map( ) : Map() {};
+    hash_map( ) : Arena(0),  Size(0), Map(){};
     hash_map( memory_arena* aArena, u32 aSize ) : Arena(aArena),  Size(aSize), Map(aArena, aSize){};
 
     void Insert( const char Key[], const T& Value)
@@ -274,6 +527,7 @@ class hash_map
       li->InsertAfter(Item);
     }
 
+    // Returns pointer to the object
     T* Get(const char* Key)
     {
       u32 Hash = djb2_hash(Key);
